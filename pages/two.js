d237156a1983deb0316387b6fa8dc316214048bb
nant0313 <nant0313@gmail.com>
2021-03-19 16:41:58 +0900

    Add Css Directly Into Component

--- pages/two.js
+++ pages/two.js
@@ -1,21 +1,36 @@
 import Image from 'next/image'
-
-import Head from 'next/head'i
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import classNames from 'classnames';
+const type = 'success'
 const Two = () => {
-    return(
-        <>
-        <Head>
-            <title>페이지 투!</title>
-        </Head>
-        <div>
-            <div style={{ height: `2000px` }}>미안..</div>
-            <Image
-                src="/favicon.ico"
-                width="10px"
-                height="10px"
-            />
-        </div>
-        </>
+    return (
+        <Layout>
+            <Head>
+                <title>페이지 투!</title>
+            </Head>
+            <div>
+                <div
+                    className={
+                        classNames({
+                            ["class1"]: type === "success",
+                            ["class2"]: true
+                        })
+                    }
+                    style={{ height: `2000px` }}>미안..</div>
+                <Image
+                    src="/favicon.ico"
+                    width="10px"
+                    height="10px"
+                />
+            </div>
+            <style jsx>{
+                `img{
+                   border: 1px solid red;
+               }`
+            }
+            </style>
+        </Layout>
     )
 }
 
