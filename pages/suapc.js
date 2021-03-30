b807b67cdf228909da456784e2ccc126d3c5a6bc
dongzoolee <nant0313@gmail.com>
2021-03-30 14:02:52 +0900

    Create Suapc Page Component

---
+++ pages/suapc.js
@@ -0,0 +1,18 @@
+import Layout from '../components/layout'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Suapc = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>SUAPC 2021 | ICPC Sinchon</title>
+            </Head>
+            <>
+                <Header />
+            </>
+        </Layout>
+    )
+}
+
+export default Suapc;
