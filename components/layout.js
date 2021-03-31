01cef15d54a91df089df336a4b4c9600fb87d549
dongzoolee <nant0313@gmail.com>
2021-03-31 13:19:25 +0900

    Add Footer

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import styles from './Layout.module.css'
 import Header from './Header'
+import Footer from './Footer'
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
             <main className={styles.main}>{children}</main>
+            <Footer />
         </>
     )
 }
