d8bac1480f8f0718d32ef39794cfec810da16514
dongzoolee <nant0313@gmail.com>
2021-03-31 13:19:19 +0900

    Create Footer Component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Footer_ = styled.footer`
+    height:100px;
+    background-color: #009D3E;
+`
+
+const Footer = () => {
+    return (
+        <Footer_ style={{ height: `100px` }} />
+    )
+}
+
+export default Footer;
