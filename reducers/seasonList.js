e90c47748cc17024aa2e76eeb0f554b78db24b1d
dongzoolee <nant0313@gmail.com>
2021-04-02 03:21:06 +0900

    Add setState Method

--- reducers/seasonList.js
+++ reducers/seasonList.js
@@ -1,8 +1,16 @@
-import seasonList from '../public/history/suapc/list.json'
+import seasonList from '../public/history/halloffame/list.json'
 
+export const SET_SL = `SET_SL`
+export const setSeasonList = (seasonList) => ({
+    type: SET_SL,
+    val: seasonList
+})
 
 const reducer = (state = seasonList, action) => {
-    return state
+    if (action.type === SET_SL)
+        return action.val
+    else
+        return state
 }
 
 export default reducer
