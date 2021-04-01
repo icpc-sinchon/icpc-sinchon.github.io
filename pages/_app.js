f551374b353b9e40a9f5286a52ad56905c5eee8f
dongzoolee <nant0313@gmail.com>
2021-04-02 03:18:58 +0900

    Initialize State Values

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from '../reducers';
 
 import '../styles/global.scss'
+import { useRouter } from 'next/router'
+
 import '../styles/SeasonNav.scss'
 import '../public/fonts/fonts.css'
 
@@ -13,19 +15,29 @@ const App = ({ Component, pageProps, store }) => {
     // 버전이 달라서 Provider로 감쌀 필요 없다 !!
     // https://darrengwon.tistory.com/557
     // <Provider store={store}>
-      <Component {...pageProps} />
+    <Component {...pageProps} />
     // </Provider>
   )
 }
 
-const configStore = (initialState, options) => {
+const configStore = (options) => {
+  const router = useRouter()
   const middleWares = []
   const enhancer = process.env.NODE_ENV === 'production' ?
     compose(applyMiddleware(...middleWares)) :
     composeWithDevTools(
       applyMiddleware(...middleWares)
     );
+  const data = require(`../public/history/${router.pathname.substring(1)}/${process.env.NEXT_PUBLIC_CURRENT_SUAPC_SEASON}.json`)
+  const seasonList = require(`../public/history/${router.pathname.substring(1)}/list.json`)
 
+  const initialState = {
+    currentSeasonData: data,
+    currentYear: data.year,
+    currentSeason: data.season,
+    currentSeasonIdx: seasonList.indexOf(process.env.NEXT_PUBLIC_CURRENT_SUAPC_SEASON),
+    seasonList
+  }
   return createStore(reducer, initialState, enhancer);
 }
 
