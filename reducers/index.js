3077c2d6fb0a163bca97591dea5682209d7fc594
dongzoolee <nant0313@gmail.com>
2021-04-02 03:21:48 +0900

    Add Newly Added Redux States

--- reducers/index.js
+++ reducers/index.js
@@ -3,10 +3,11 @@ import currentSeasonIdx from './currentSeasonIdx'
 import currentSeasonData from './currentSeasonData'
 import seasonList from './seasonList'
 import currentYear from './currentYear'
+import currentSeason from './currentSeason'
 
 
 const rootReducer = combineReducers({
-    currentSeasonIdx, currentSeasonData, seasonList, currentYear
+    currentSeasonIdx, currentSeasonData, seasonList, currentYear, currentSeason
 })
 
 export default rootReducer;
