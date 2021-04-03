d51dff76aabc3897e4b4b627ef218b36bcb1bd63
dongzoolee <nant0313@gmail.com>
2021-04-03 10:29:04 +0900

    Create ArchiveButton Component

---
+++ components/ArchiveButton/ArchiveButton.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const CustomButton = styled.div`
+    border: 2px solid #009D3E;
+    
+    padding: 0.4rem 1.2rem;
+    
+    font-size: 0.8rem;
+    font-weight: 700;
+    color: #009D3E;
+`
+
+const ATag = styled.a`
+    margin-left: 1.2rem;
+        
+    &:first-child{
+        margin-left: 0;
+    }
+
+    &:hover{
+        text-decoration: none;
+    }
+`
+
+const ArchiveButton = ({ href, children }) => {
+    return (
+        <ATag href={href} target="_blank">
+            <CustomButton>
+                {children}
+            </CustomButton>
+        </ATag>
+    )
+}
+
+export default ArchiveButton
