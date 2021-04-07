f8226436350d6717769304c531e8f374f8c5e31d
dongzoolee <nant0313@gmail.com>
2021-04-08 03:30:31 +0900

    Set Mobile Footer Design

--- components/Footer.js
+++ components/Footer.js
@@ -12,6 +12,14 @@ const Footer_ = styled.footer`
 
     color: white;
     background-color: #009D3E;
+
+    @media(max-width: 900px){
+        height: auto;
+    }
+
+    @media(max-width: 500px){
+        padding: 2.4rem 2rem 3rem 2rem;
+    }
 `
 
 const BottomWrap = styled.div`
@@ -19,14 +27,34 @@ const BottomWrap = styled.div`
     
     justify-content: space-between;
     align-items: flex-end;
+
+    @media(max-width: 900px){
+        display: block;
+        margin-top: 4rem;
+    }
 `
 
 const OrganizerWrap = styled.div`
+    font-size: 0.9rem;
 
+    word-break: keep-all;
+    span{
+        &::after{
+            padding: 0 6px;
+            content: "|";
+        }
+    }
+`
+
+const OrgTitle = styled.div`
+    font-size: 0.9rem;
+    font-weight: 700;
 `
 
 const CIWrap1 = styled.div`
-
+    @media(max-width: 900px){
+        margin-top: 2rem;
+    }
 `
 const CIWrap2 = styled.div`
     display: flex;
@@ -44,6 +72,7 @@ const CIWrap = styled.div`
 
 const CopyRight = styled.div`
     font-size: 0.6rem;
+    text-align: right;
 `
 
 const IconWrap = styled.div`
@@ -74,10 +103,30 @@ const Footer = () => {
                 </IconWrap>
             </TopWrap>
             <BottomWrap>
-                <OrganizerWrap>운영진</OrganizerWrap>
-                {organizer[0].member.map(person=>{
-                    return person.name
-                })}
+                <OrganizerWrap>
+                    <OrgTitle>회장</OrgTitle>
+                    <div>
+                        {organizer[0].president.map((person, idx) => {
+                            if (idx != organizer[0].president.length - 1)
+                                return (
+                                    <span>{person.name}</span>
+                                )
+                            else
+                                return person.name
+                        })}
+                    </div>
+                    <OrgTitle style={{ marginTop: '0.2rem' }}>운영진</OrgTitle>
+                    <div>
+                        {organizer[0].member.map((person, idx) => {
+                            if (idx != organizer[0].member.length - 1)
+                                return (
+                                    <span>{person.name}</span>
+                                )
+                            else
+                                return person.name
+                        })}
+                    </div>
+                </OrganizerWrap>
                 <CIWrap1>
                     <CIWrap2>
                         <div>ICPC Sinchon</div>
