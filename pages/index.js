30f186253857c804ba4fac8d66a1cff44bff9540
dongzoolee <nant0313@gmail.com>
2021-04-09 15:41:11 +0900

    Add Media Query & ClassNames

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ const NotoSansBold = css`
 const MainBottom = css`
     display: flex;
     justify-content: space-between;
+    flex-wrap: wrap;
 
     margin: 2.4rem 0;
 `
@@ -23,24 +24,24 @@ const Main = () => {
                 <title>Main | ICPC Sinchon</title>
             </Head>
             <MainWrap>
-                <ContentWrap>
+                <ContentWrap className="content-wrap-main">
                     <TextTitle>ICPC Sinchon</TextTitle>
                     <TextSubTitle>신촌지역 대학교 프로그래밍 대회 동아리 연합</TextSubTitle>
                 </ContentWrap>
                 <MainImageWrap>
 
                 </MainImageWrap>
-                <ContentWrap css={MainBottom}>
-                    <ContentItem>
+                <ContentWrap css={MainBottom} className="content-wrap-main">
+                    <ContentItem className="ContentItem">
                         <ContentTitle>소개</ContentTitle>
                         <ContentText>
-                            신촌지역 대학교 프로그래밍 동아리 연합입니다.
+                            신촌지역 대학교 프로그래밍 대회 동아리 연합입니다.
                             서강대학교, 숙명여자대학교, 연세대학교,
                             이화여자대학교, 홍익대학교 프로그래밍
                             동아리가 연합하여 활동중입니다.
                             </ContentText>
                     </ContentItem>
-                    <ContentItem>
+                    <ContentItem className="ContentItem">
                         <ContentTitle>소속 학교</ContentTitle>
                         <SchoolWrap>
                             <SchoolItem>
@@ -65,8 +66,22 @@ const Main = () => {
                             </SchoolItem>
                         </SchoolWrap>
                     </ContentItem>
-                    <ContentItem>
+                    <ContentItem className="ContentItem ContentItem-PC">
                         <ContentTitle>활동</ContentTitle>
+                        <ContentText>
+                            매 시즌 프로그래밍 대회를 개최하며 알고리즘 캠프를 통해
+                            연합원들에게 알고리즘 공부의 기회를 제공합니다.
+                            </ContentText>
+                    </ContentItem>
+                    <ContentItem className="ContentItem ContentItem-Tablet">
+                        <ContentTitle>SUAPC</ContentTitle>
+                        <ContentText>
+                            매 시즌 프로그래밍 대회를 개최하며 알고리즘 캠프를 통해
+                            연합원들에게 알고리즘 공부의 기회를 제공합니다.
+                            </ContentText>
+                    </ContentItem>
+                    <ContentItem className="ContentItem ContentItem-Tablet">
+                        <ContentTitle>Algorithm Camp</ContentTitle>
                         <ContentText>
                             매 시즌 프로그래밍 대회를 개최하며 알고리즘 캠프를 통해
                             연합원들에게 알고리즘 공부의 기회를 제공합니다.
@@ -131,6 +146,10 @@ const ContentText = styled.p`
 const SchoolWrap = styled.p`
     display: flex;
     justify-content: space-between;
+
+    @media(max-width:1135px){
+        justify-content: space-evenly;
+    }
 `
 
 const SchoolItem = styled.div`
