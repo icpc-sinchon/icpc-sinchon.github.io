db52887782be399b823866a5e8c8133ce569dcf9
dongzoolee <nant0313@gmail.com>
2021-04-11 21:20:44 +0900

    Fix Error

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import NavSVG from '../components/HeaderNavSVG'
