c43f674a063b05305f894e82a1adde9afa75d0d2
dongzoolee <nant0313@gmail.com>
2021-04-11 21:05:21 +0900

    Add NavBarWindow

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,101 @@
 import styled from 'styled-components'
 import Link from 'next/link'
-
+import { useEffect, useState } from 'react'
+import NavSVG from '../components/HeaderNavSVG'
+import LogoSVG from '../components/HeaderLogoSVG'
 
 const Header = () => {
+    const [isNavClicked, setIsNavClicked] = useState(false)
+
+    const onNavClick = () => {
+        if (isNavClicked)
+            setIsNavClicked(false)
+        else
+            setIsNavClicked(true)
+    }
+
+    const LogoImgWrap = styled.div`
+        width:36px;
+        z-index: 10002;
+
+        cursor: pointer;
+
+        circle{
+            ${isNavClicked ? "fill: white;" : ""}
+        }
+
+        path{
+            ${isNavClicked ? "fill: #009D3E;" : ""}
+        }
+
+        @media(max-width: 700px){
+            width:32px;
+        }
+    `
+
+    const NavBarWrap = styled.div`
+        display: none;
+        z-index: 10002;
+
+        path{
+            ${isNavClicked ? "fill: white;" : ""}
+        }
+
+        @media(max-width: 500px){
+            display: block;
+        }
+    `
+
+    useEffect(() => {
+        document.querySelector("main").addEventListener("click", e => {
+            setIsNavClicked(false);
+        })
+        document.querySelector("footer").addEventListener("click", e => {
+            setIsNavClicked(false);
+        })
+    }, [])
+
     return (
-        <HeaderWrap>
-            <Link href="/">
-                <LogoImgWrap>
-                    <LogoImg src="/res/header/240logo.svg" />
-                </LogoImgWrap>
-            </Link>
-            <div style={{ flexGrow: "1" }} />
-            <Link href="/suapc"><HeaderElem style={{ color: '#009d3e' }}>SUAPC 2021</HeaderElem></Link>
-            <Link href="/halloffame"><HeaderElem>명예의 전당</HeaderElem></Link>
-            <Link href="/sponser"><HeaderElem>후원 및 협업</HeaderElem></Link>
-            <Link href="/contact"><HeaderElem>문의하기</HeaderElem></Link>
-            <NavBarWrap>
-                <LogoImg src="res/header/navbar.svg" />
-            </NavBarWrap>
-        </HeaderWrap>
+        <>
+            <HeaderWrap>
+                {isNavClicked ? <NavBarWindow>
+                    <Link href="/suapc"><NavBarElem>SUAPC 2021</NavBarElem></Link>
+                    <Link href="/halloffame"><NavBarElem>명예의 전당</NavBarElem></Link>
+                    <Link href="/sponser"><NavBarElem>후원 및 협업</NavBarElem></Link>
+                    <Link href="/contact"><NavBarElem>문의하기</NavBarElem></Link>
+                </NavBarWindow> : ""}
+                <Link href="/">
+                    <LogoImgWrap>
+                        <LogoSVG />
+                    </LogoImgWrap>
+                </Link>
+                <div style={{ flexGrow: "1" }} />
+                <Link href="/suapc"><HeaderElem style={{ color: '#009d3e' }}>SUAPC 2021</HeaderElem></Link>
+                <Link href="/halloffame"><HeaderElem>명예의 전당</HeaderElem></Link>
+                <Link href="/sponser"><HeaderElem>후원 및 협업</HeaderElem></Link>
+                <Link href="/contact"><HeaderElem>문의하기</HeaderElem></Link>
+                <NavBarWrap onClick={onNavClick}>
+                    <NavSVG />
+                </NavBarWrap>
+            </HeaderWrap>
+        </>
     )
 }
+
+const NavBarWindow = styled.div`
+    position: fixed;
+    z-index: 10001;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 190px;
+    padding-top: 75px;
+
+    background: #009D3E;
+
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
+`
 
 const HeaderWrap = styled.header`
     display:flex;
@@ -38,19 +113,6 @@ const HeaderWrap = styled.header`
     @media(max-width: 700px){
         padding: 0 8vw;
     }
-`
-const LogoImgWrap = styled.div`
-    width:36px;
-
-    cursor: pointer;
-    @media(max-width: 700px){
-        width:32px;
-    }
-`
-
-const LogoImg = styled.img`
-    width:100%;
-    height:100%;
 `
 
 const HeaderElem = styled.div`
@@ -77,10 +139,18 @@ const HeaderElem = styled.div`
     }
 `
 
-const NavBarWrap = styled.div`
-    display: none;
-    @media(max-width: 500px){
-        display: block;
-    }
+const NavBarElem = styled.div`
+    font-family: 'KeepCalmMed', 'Noto Sans KR';
+    font-size: 0.8rem;
+    font-weight: 500;
+    color: white;
+    letter-spacing: -0.02rem;
+
+    text-align: right;
+
+    padding: 0 8vw;
+
+    cursor: pointer;
 `
+
 export default Header;
