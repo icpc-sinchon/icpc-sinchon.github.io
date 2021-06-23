6a2d64f44068d0921a10e384b65638c7cb0553e5
dongzoolee <nant0313@gmail.com>
2021-06-23 12:48:30 +0900

    Add AdmissionButton

---
+++ components/AdmissionButton/AdmissionButton.js
@@ -0,0 +1,41 @@
+import styled, { css } from 'styled-components'
+
+const CustomButton = styled.div`
+    ${'' /* border: 2px solid #009D3E; */}
+    
+    padding: 0.4rem 1.2rem;
+    
+    font-family: 'Noto Sans KR';
+    font-size: 0.8rem;
+    font-weight: 700;
+    ${({ isDeprecated }) =>
+        isDeprecated ?
+            css`background-color: #808080;`
+            : css`background-color: #009D3E;`
+    }
+    color:white;
+`
+
+const ATag = styled.a`
+    margin-left: 1.2rem;
+        
+    &:first-child{
+        margin-left: 0;
+    }
+
+    &:hover{
+        text-decoration: none;
+    }
+`
+
+const AdmissionButton = ({ href, isDeprecated, children }) => {
+    return (
+        <ATag href={href} target="_blank">
+            <CustomButton isDeprecated={isDeprecated}>
+                {children}
+            </CustomButton>
+        </ATag>
+    )
+}
+
+export default AdmissionButton
