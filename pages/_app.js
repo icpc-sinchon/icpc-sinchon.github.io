49dd1f0752a25cc8c56f2943275e8bb2e6ccbca1
nant0313 <nant0313@gmail.com>
2021-12-01 11:47:26 +0900

    Fix logic: render different season based on env

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import { createWrapper } from 'next-redux-wrapper'
-import { applyMiddleware, compose, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import reducer from '../reducers';
+import { createWrapper } from "next-redux-wrapper";
+import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from "../reducers";
 
-import '../styles/global.scss'
-import '../styles/SeasonNav.scss'
-import '../public/fonts/fonts.css'
-import '../components/Arrow_Preview.scss'
-import '../components/ContestWrap.scss'
-import '../pages/index.scss'
+import "../styles/global.scss";
+import "../styles/SeasonNav.scss";
+import "../public/fonts/fonts.css";
+import "../components/Arrow_Preview.scss";
+import "../components/ContestWrap.scss";
+import "../pages/index.scss";
 
 const App = ({ Component, pageProps, store }) => {
   return (
@@ -17,39 +17,53 @@ const App = ({ Component, pageProps, store }) => {
     // <Provider store={store}>
     <Component {...pageProps} />
     // </Provider>
+  );
+};
+
+const configStore = (ctx) => {
+  const middleWares = [];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middleWares))
+      : composeWithDevTools(applyMiddleware(...middleWares));
+
+  if (
+    !ctx.router ||
+    (ctx.router.pathname !== "/suapc" &&
+      ctx.router.pathname !== "/halloffame" &&
+      ctx.router.pathname !== "/campcontest")
   )
-}
+    return createStore(reducer, enhancer);
 
-const configStore = ctx => {
-  const middleWares = []
-  const enhancer = process.env.NODE_ENV === 'production' ?
-    compose(applyMiddleware(...middleWares)) :
-    composeWithDevTools(
-      applyMiddleware(...middleWares)
-    );
+  let pathname = ctx.router.pathname;
+  if (pathname === "/campcontest") pathname = "/halloffame";
 
-  if (!ctx.router || (ctx.router.pathname !== '/suapc' && ctx.router.pathname !== '/halloffame' && ctx.router.pathname !== '/campcontest')) return createStore(reducer, enhancer)
+  let currentSeason = process.env.NEXT_PUBLIC_CURRENT_SUAPC_SEASON;
+  if (pathname === "/halloffame") {
+    currentSeason = process.env.NEXT_PUBLIC_CURRENT_HALLOFFAME_SEASON;
+  }
 
-  let pathname = ctx.router.pathname
-  if (pathname === '/campcontest') pathname = '/halloffame'
-
-  const data = require(`../public/history/${pathname.substring(1)}/${process.env.NEXT_PUBLIC_CURRENT_SUAPC_SEASON}.json`)
-  const seasonList = require(`../public/history/${pathname.substring(1)}/list.json`)
+  const data = require(`../public/history/${pathname.substring(
+    1
+  )}/${currentSeason}.json`);
+  const seasonList = require(`../public/history/${pathname.substring(
+    1
+  )}/list.json`);
 
   const initialState = {
     currentSeasonData: data,
     currentYear: data.year,
     currentSeason: data.season,
-    currentSeasonIdx: seasonList.indexOf(process.env.NEXT_PUBLIC_CURRENT_SUAPC_SEASON),
+    currentSeasonIdx: seasonList.indexOf(currentSeason),
     seasonList,
-    isNavClicked: false
-  }
+    isNavClicked: false,
+  };
+
   return createStore(reducer, initialState, enhancer);
-}
+};
 
 // 전역적으로 props에 context를 넣어준다
-App.getInitialProps = async context => {
-
+App.getInitialProps = async (context) => {
   const { ctx, Component } = context;
 
   let pageProps = {};
@@ -58,8 +72,8 @@ App.getInitialProps = async context => {
   }
 
   return { pageProps };
-}
+};
 
 // store Props가 withRedux를 통해 주입된다.
-export default createWrapper(configStore).withRedux(App)
+export default createWrapper(configStore).withRedux(App);
 // export default withRedux(configStore)(App)
