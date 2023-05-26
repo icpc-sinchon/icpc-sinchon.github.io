e618d1a861af32d1eef2f0c8948ea767f2634299
DongZoo Lee <dongzoo@kodebox.io>
2023-05-26 23:42:52 +0900

    Change the page file naming rule
    From .js to .page.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   exportPathMap: function () {
     return {
       "/": { page: "/" },
