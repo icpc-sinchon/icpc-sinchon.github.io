ae2da8646686c9ef571aab0aaf5ac68438ede60d
derug <derug@users.noreply.github.com>
2023-10-09 08:38:30 +0000

    deploy: 46b461c0b2490e530b5b2f35f07ac00e51236460

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "99404b5c1b759113f1b7";
+/******/ 	var hotCurrentHash = "8c853581e74a395a84ca";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
